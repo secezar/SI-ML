f0719873abe972b60e01b8503e3fadf1eebc0fed
secezar <piotrjanyst95@gmail.com>
2017-06-13 15:49:38 +0200

    Splitted train and model initialization

--- src/cnn_lstm.py
+++ src/cnn_lstm.py
@@ -40,19 +40,7 @@ class CNNLSTM():
     Only 2 epochs are needed as the dataset is very small.
     '''
     def initialize(self):
-        print('Loading data...')
-        (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=self.max_features)
-        print(len(x_train), 'train sequences')
-        print(len(x_test), 'test sequences')
-
-        print('Pad sequences (samples x time)')
-        x_train = sequence.pad_sequences(x_train, maxlen=self.maxlen)
-        x_test = sequence.pad_sequences(x_test, maxlen=self.maxlen)
-        print('x_train shape:', x_train.shape)
-        print('x_test shape:', x_test.shape)
-
         print('Build model...')
-
         model = Sequential()
         model.add(Embedding(self.max_features, self.embedding_size, input_length=self.maxlen))
         model.add(Dropout(0.25))
@@ -69,6 +57,19 @@ class CNNLSTM():
         model.compile(loss='binary_crossentropy',
                       optimizer='adam',
                       metrics=['accuracy'])
+
+    def train(self, model):
+        print('Loading data...')
+        (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=self.max_features)
+        print(len(x_train), 'train sequences')
+        print(len(x_test), 'test sequences')
+
+        print('Pad sequences (samples x time)')
+        x_train = sequence.pad_sequences(x_train, maxlen=self.maxlen)
+        x_test = sequence.pad_sequences(x_test, maxlen=self.maxlen)
+        print('x_train shape:', x_train.shape)
+        print('x_test shape:', x_test.shape)
+
 
         print('Train...')
         model.fit(x_train, y_train,
